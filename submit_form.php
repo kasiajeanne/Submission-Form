c4f4bd9870cbff3d9780b6430305c4d953b4a116
snugs <81722517+kasiajeanne@users.noreply.github.com>
2024-11-13 09:19:39 -0500

    error reporting

--- submit_form.php
+++ submit_form.php
@@ -1,4 +1,9 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+ini_set('log_errors', 1);
+ini_set('error_log', __DIR__ . '/error_log.txt');  // Saves errors to error_log.txt in the same directory
 require 'vendor/autoload.php';
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
