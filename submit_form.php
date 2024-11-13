7f2ca12126a3bc2c9426faa06d7790e719964448
snugs <81722517+kasiajeanne@users.noreply.github.com>
2024-11-13 09:56:22 -0500

    Took code out for public

--- submit_form.php
+++ submit_form.php
@@ -38,7 +38,7 @@ try {
     $mail->Host = 'smtp.gmail.com';
     $mail->SMTPAuth = true;
     $mail->Username = 'kasia@enablemusic.group';
-    $mail->Password = 'tknj nqqq nkgm fyhb';
+    $mail->Password = '';
     $mail->SMTPSecure = 'tls';
     $mail->Port = 587;
 
